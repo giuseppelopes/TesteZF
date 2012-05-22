b8d74102173aeda0599e90222a2eafe9891a6548
Giuseppe <giuseppe.lopes@kinghost.com.br>
2012-05-22 10:18:01 -0300

    Adicionado suite de AllTest

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
-
+echo(APPLICATION_PATH);
 // Define application environment
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'testing'));
